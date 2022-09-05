c7e683b84b4dc2847ca0de839986c64a56f63bdd
Santiago Scalise <santi.scalise@gmail.com>
2022-09-05 15:04:11 +0200

    added no args constructor

--- src/main/java/com/telefonica/rickandmorty/model/Episode.java
+++ src/main/java/com/telefonica/rickandmorty/model/Episode.java
@@ -3,8 +3,10 @@ package com.telefonica.rickandmorty.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Episode {
     private Integer id;
